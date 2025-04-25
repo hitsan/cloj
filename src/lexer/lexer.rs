eca51e94f7304266aaedebe2bbb3e84028b116ea
hitsan <soledewa2828@gmail.com>
2025-04-26 00:35:42 +0900

    implement token

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,8 +1,8 @@
-use crate::lexer::token;
+use crate::lexer::token::Token;
 
-fn lex_num(code: &str) -> Option<i32> {
+fn lex_num(code: &str) -> Option<Token> {
     match code.parse() {
-        Ok(n) => Some(n),
+        Ok(n) => Some(Token::Number(n)),
         Err(_) => None,
     }
 }
@@ -15,15 +15,14 @@ mod tests {
     fn test_num() {
         let num = "1".to_string();
         let result = lex_num(&num);
-        assert_eq!(result, Some(1));
+        assert_eq!(result, Some(Token::Number(1)));
 
         let num = "11".to_string();
         let result = lex_num(&num);
-        assert_eq!(result, Some(11));
+        assert_eq!(result, Some(Token::Number(11)));
 
         let num = "k".to_string();
         let result = lex_num(&num);
         assert_eq!(result, None);
-
     }
 }
